8672eb33bcc359794d5a23f7579aa740cdf9ab95
Timo van Zuylen <timo.van.zuylen@hva.nl>
2025-02-04 18:54:51 +0100

    added manager for tiles and changed the asset for ground and platforms

---
+++ lib/managers/segmentManager.dart
@@ -0,0 +1,15 @@
+import 'package:flame/game.dart';
+
+class Block {
+  final Vector2 gridPosition;
+  final Type blockType;
+  Block(this.gridPosition, this.blockType);
+}
+
+final segments = [
+  segment0,
+];
+
+final segment0 = [
+
+];
